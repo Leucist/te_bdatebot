8b12c608464e2e2dfc88022548c85067777ef656
Leucist <vlad2ka@tut.by>
2021-07-03 22:13:00 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Telegram BDateBot
+> v = 0.1\
+> Author: [leucist](https://github.com/Leucist)
+
+This is an [aiogram](https://docs.aiogram.dev/en/latest/) Python bot for Telegram\
+The aim of this bot is to simply inform when some of your friends have birthday that day (or later).\
+Users are able to add their friends' information to the database (unique for each user) through the bot and then get notified when someone from the list has birthday.
+```diff 
+! Heroku, the server I will probably use, 
+! doesn't support adequate data saving in databases.
+! JSON as well. So they way of data saving shall be changed later
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.bot import api
+from config import admin_id, TOKEN
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def start(message: types.Message):
+    if message.text:
+        await message.answer("Привет! Я бот, который может помочь тебе держать под контролем дни рождения всех друзей и знакомых, не забыв никого!")
+        await bot.send_message(message.chat.id, 'Чтобы добавить человека в список, напиши мне "/add"\nЧтобы изменить информацию о ком-нибудь, напиши "/change"\nЧтобы просмотреть ближайшие дни рождения, напиши "/check"')
+    else:
+        await message.answer("Пожалуйста, пришлите мне текстовое сообщение")
+
+
+@dp.message_handler(commands=['add'])
+async def add():
+    pass
+
+
+@dp.message_handler(commands=['change'])
+async def change():
+    pass
+
+
+@dp.message_handler(commands=['check'])
+async def check():
+    pass
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
